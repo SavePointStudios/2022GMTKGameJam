efa5820b41b94dc10332e4d4851963ed317fef59
xGauss05 <jcacaynes@gmail.com>
2022-07-15 23:40:38 +0200

    Implemented ModuleBreakables. - Player should have an ability to destroy them. (Collider type Player shot break) - Regular shots don't destroy them. - Player and enemies shouldn't pass over them. - Player and enemy shots disappear when colliding against them

--- Project_9_Solution/Source/Breakable_Chips.cpp
+++ Project_9_Solution/Source/Breakable_Chips.cpp
@@ -7,28 +7,16 @@
 #include "ModulePlayer.h"
 
 Breakable_Chips::Breakable_Chips(int x, int y, unsigned short version) : Breakable(x, y) {
-	collider = App->collisions->AddCollider({ 0, 0, 32, 32 }, Collider::Type::BREAKABLE, (Module*)App->breakables);
+	collider = App->collisions->AddCollider({ 0, 0, 24, 24 }, Collider::Type::BREAKABLE, (Module*)App->breakables);
+	//destroyedFx = App->audio->LoadFx();
 	switch (version) {
+	// in case if we want different chips with diff texture
 	case 1:
 		animation.PushBack({ 0,96,32,32 });
 		break;
-	case 2:
-		animation.PushBack({ 32,96,32,32 });
-		break;
-	case 3:
-		animation.PushBack({ 64,96,32,32 });
-		break;
-	case 4:
-		animation.PushBack({ 96,96,32,32 });
-		break;
-	case 5:
-		animation.PushBack({ 0,64,32,32 });
-		break;
-	case 6:
-		animation.PushBack({ 32,64,32,32 });
-		break;
+	
 	default:
-		animation.PushBack({ 0,96,32,32 });
+		animation.PushBack({ 5,72,21,22 });
 		break;
 	}
 
