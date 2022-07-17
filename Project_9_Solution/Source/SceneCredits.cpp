0ad6e52338cfc127071d2d9b28b211192dc89fcf
xGauss05 <jcacaynes@gmail.com>
2022-07-17 20:40:59 +0200

    Credits implemented.

--- Project_9_Solution/Source/SceneCredits.cpp
+++ Project_9_Solution/Source/SceneCredits.cpp
@@ -22,20 +22,22 @@ bool SceneCredits::Start() {
 	LOG("Loading background assets");
 	bool ret = true;
 
-	//bgTexture = App->textures->Load("");
+	bgTexture = App->textures->Load("Assets/Sprites/credits.png");
 
 	//App->audio->PlayMusic("Assets/Music/Menu.ogg", 1.0f);
 	char lookupTable[] = { "! @,_./0123456789$;< ?abcdefghijklmnopqrstuvwxyz" };
 	fontId = App->fonts->Load("Assets/Fonts/casino_font_black.png", lookupTable, 1);
-
+	imgYPosition = 0;
 	return ret;
 }
 
 Update_Status SceneCredits::Update() {
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
+	imgYPosition--;
 
-	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN) {
+	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN ||
+		imgYPosition <= -1440) {
 		App->fade->FadeToBlack(this, (Module*)App->sceneMenu, 90);
 	}
 	return Update_Status::UPDATE_CONTINUE;
@@ -45,7 +47,7 @@ Update_Status SceneCredits::Update() {
 Update_Status SceneCredits::PostUpdate() {
 	// Draw everything --------------------------------------
 
-	//App->render->Blit(bgTexture, App->render->camera.x, App->render->camera.y, NULL);
+	App->render->Blit(bgTexture, 0, imgYPosition, NULL);
 
 	return Update_Status::UPDATE_CONTINUE;
 }
